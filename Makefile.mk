101a95f7ab170d3b8213f5e19f9323041caeccf9
Vladislav <113616192+ImMoRTaLex@users.noreply.github.com>
2022-11-07 19:20:51 +0300

    Update Makefile.mk

--- Makefile.mk
+++ Makefile.mk
@@ -1,4 +1,4 @@
 all: 
-	gcc -Wall -g Task_Without_Pthreads.c -o Task_Without_Pthreads -lm
-	gcc -Wall -g Task_With_Pthreads.c -o Task_With_Pthreads -lm
-
+	gcc -Wall -g Task-1.31.c -o Task-1.31 -lm
+	gcc -Wall -g Task-1.32.c -o Task-1.32 -lm
+	gcc -Wall -g Task-1.48.c -o Task-1.48 -lm
