cbec28bf562655d1497345cb906c16ae500c9822
Vladislav <113616192+ImMoRTaLex@users.noreply.github.com>
2022-11-07 18:55:05 +0300

    Update Task-1.48.c

--- Task-1.48/Task-1.48.c
+++ Task-1.48/Task-1.48.c
@@ -7,9 +7,9 @@ int main (){
   if (n == 0) min = max = 0;
   printf ("Введите числа:\n");
   for (i = 0; i < n; i++){
-    scanf ("%d", x);
+    scanf ("%d", &x);
     if (x > max) max = x;
-    if (x < min) x = min;
+    if (x < min) min = x;
   }
   printf ("Был введён ряд из %d чисел, min = %d, max = %d\n", n, min, max);
   return 0;
