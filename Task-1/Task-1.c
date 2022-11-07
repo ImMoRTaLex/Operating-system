c56510018fd787580a134010e30de5b59f7c4f74
Vladislav <113616192+ImMoRTaLex@users.noreply.github.com>
2022-11-07 18:30:32 +0300

    Update Task-1.c

--- Task-1/Task-1.c
+++ Task-1/Task-1.c
@@ -1,1 +1,24 @@
-in
+#include <stdio.h>
+
+int main(){
+  int a, min, max;
+  char b = 'n';
+  min = 1;
+  max = 200;
+  a = (max + min) / 2;
+  do{
+    printf ("Загаданное число %d?\n", a);
+    scanf ("%c", &b);
+    if (b == '>'){
+      min = a;
+    }
+      a = (max + min)/2;
+    if (b == '<'){
+      max = a;
+      a = (max + min)/2;
+    }
+  }
+  while (b != '=');
+  printf ("Ура, победа!\n");
+  return 0;
+}
